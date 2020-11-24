88322387f0817a5ac7e3c4fb72bc47c42918beab
vologiroff <volkant0778@ya.ru>
2020-11-25 01:27:06 +0300

    initial
    - Flutter Package Manager used
    - AudioCache function observed

---
+++ android/app/src/main/kotlin/com/kantemirvologirov/xylophone_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kantemirvologirov.xylophone_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+void playSound(int soundNumber) {
+  final player = AudioCache();
+  player.play('note$soundNumber.wav');
+}
+
+Expanded buildKey({Color color, int soundNumber}) {
+  return Expanded(
+    child: FlatButton(
+      color: color,
+      onPressed: () {
+        playSound(soundNumber);
+      },
+    ),
+  );
+}
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
